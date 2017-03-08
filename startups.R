693839e73c50647a0f5380c76a67b3cc19ccf51a
Saurav  Kharb <TheSauravKharb@D-10-157-130-54.dhcp4.washington.edu>
2017-03-08 03:56:11 -0800

    Add funding rounds to code

--- startups.R
+++ startups.R
@@ -2,10 +2,18 @@
 library("dplyr")
 cb_ipos <- read.csv("cb_ipos.csv", quote = "", stringsAsFactors = FALSE)
 cb_funding.rounds <- read.csv("cb_funding_rounds.csv", quote = "", stringsAsFactors = FALSE)
+cb_relationships <- read.csv("cb_relationships.csv", quote = "", stringsAsFactors = FALSE)
 
 
 funding.rounds.c <- filter(cb_funding.rounds, X.funding_round_type. == "'series-c+'") %>% 
   select(3)
+
+funding.rounds.b <- filter(cb_funding.rounds, X.funding_round_type. == "'series-b'") %>% 
+  select(3)
+
+funding.rounds.a <- filter(cb_funding.rounds, X.funding_round_type. == "'series-a'") %>% 
+  select(3)
+
 
 c.funding.with.ipo <- !funding.rounds.c$X.object_id. %in% cb_ipos$X.object_id.
 
