7ba0146c22305f0fb5cee7d2a95386cfab032e90
pachauri ryan <rpachauri@gmail.com>
2017-03-09 05:48:40 -0800

    add office plots

--- offices.R
+++ offices.R
@@ -1,7 +1,7 @@
 source("startups.R")
 
 #remove no longer necessary variables
-remove(cb_funding.rounds, cb_ipos, cb_relationships, data.complete, relationships, relationships.funding.a, relationships.funding.b,
+remove(cb_funding.rounds, cb_relationships, data.complete, relationships, relationships.funding.a, relationships.funding.b,
        relationships.funding.c, relationships.ipo)
 
 cb_offices <- read.csv(paste0(data.folder, "cb_offices2.csv"), quote = "", stringsAsFactors = FALSE)
@@ -9,3 +9,15 @@ cb_objects <- read.csv(paste0(data.folder, "cb_objects.csv"), quote = "", string
 cb_offices <- filter(cb_offices, X.country_code. == "'USA'", X.latitude. != "NULL") %>% 
   arrange(X.object_id.)
 
+ipo.offices <- filter(cb_offices, X.object_id. %in% cb_ipos$X.object_id.)
+series.c.offices <- filter(cb_offices, X.object_id. %in% funding.rounds.c$object_id)
+series.b.offices <- filter(cb_offices, X.object_id. %in% funding.rounds.b$object_id)
+series.a.offices <- filter(cb_offices, X.object_id. %in% funding.rounds.a$object_id)
+
+
+#ggplot(data = cb_offices) + geom_point(aes(x = X.latitude., y = X.longitude.), size = 0.5) + labs(x=NULL, y=NULL)
+
+ipo <- ggplot(data = ipo.offices) + geom_point(aes(x = X.latitude., y = X.longitude.), size = 0.5) + labs(x=NULL, y=NULL)
+c <- ggplot(data = series.c.offices) + geom_point(aes(x = X.latitude., y = X.longitude.), size = 0.5) + labs(x=NULL, y=NULL)
+b <- ggplot(data = series.b.offices) + geom_point(aes(x = X.latitude., y = X.longitude.)) + labs(x=NULL, y=NULL)
+a <- ggplot(data = series.a.offices) + geom_point(aes(x = X.latitude., y = X.longitude.)) + labs(x=NULL, y=NULL)
