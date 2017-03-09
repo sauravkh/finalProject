6f6a7e6f94956882df04275930a0e9a3863b31ad
pachauri ryan <rpachauri@gmail.com>
2017-03-09 11:27:17 -0800

    Fix map to look like USA

--- offices.R
+++ offices.R
@@ -8,19 +8,21 @@ cb_offices <- read.csv(paste0(data.folder, "cb_offices2.csv"), quote = "", strin
 cb_objects <- read.csv(paste0(data.folder, "cb_objects.csv"), quote = "", stringsAsFactors = FALSE)
 cb_offices <- filter(cb_offices, X.country_code. == "'USA'", X.latitude. != "NULL") %>% 
   arrange(X.object_id.)
+cb_offices$X.longitude. <- as.numeric(cb_offices$X.longitude.)
+cb_offices$X.latitude. <- as.numeric(cb_offices$X.latitude.)
+
+usa <- map_data("usa")
 
 ipo.offices <- filter(cb_offices, X.object_id. %in% cb_ipos$X.object_id.)
 series.c.offices <- filter(cb_offices, X.object_id. %in% funding.rounds.c$object_id)
 series.b.offices <- filter(cb_offices, X.object_id. %in% funding.rounds.b$object_id)
 series.a.offices <- filter(cb_offices, X.object_id. %in% funding.rounds.a$object_id)
 
-
-#ggplot(data = cb_offices) + geom_point(aes(x = X.latitude., y = X.longitude.), size = 0.5) + labs(x=NULL, y=NULL)
-
-ipo <- ggplot(data = ipo.offices) + geom_point(aes(x = X.latitude., y = X.longitude.), color = "black", size = 0.5) + labs(x=NULL, y=NULL) + 
-        geom_point(data = series.c.offices, aes(x = X.latitude., y = X.longitude.), color = "red", size = 0.5) + labs(x=NULL, y=NULL) + 
-        geom_point(data = series.b.offices, aes(x = X.latitude., y = X.longitude.), color = "green", size = 0.5) + labs(x=NULL, y=NULL) + 
-        geom_point(data = series.a.offices, aes(x = X.latitude., y = X.longitude.), color = "blue", size = 0.5) + labs(x=NULL, y=NULL) + 
+everything <- ggplot(data = usa) + geom_point(aes(x = long, y = lat), size = 0.1) + labs(x=NULL, y=NULL) +
+        geom_point(data = ipo.offices, aes(x = X.longitude., y = X.latitude.), color = "red", size = 0.5) + labs(x=NULL, y=NULL) + 
+        geom_point(data = series.c.offices, aes(x = X.longitude., y = X.latitude.), color = "yellow", size = 0.5) + labs(x=NULL, y=NULL) + 
+        geom_point(data = series.b.offices, aes(x = X.longitude., y = X.latitude.), color = "green", size = 0.5) + labs(x=NULL, y=NULL) + 
+        geom_point(data = series.a.offices, aes(x = X.longitude., y = X.latitude.), color = "blue", size = 0.5) + labs(x=NULL, y=NULL) + 
         theme(axis.title = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank())
+              axis.text = element_blank(),
+              axis.ticks = element_blank())
