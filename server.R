b8710f2c9f5905aeef52d82f1ed86669154c7641
Saurav  Kharb <TheSauravKharb@D-10-157-130-54.dhcp4.washington.edu>
2017-03-08 23:24:25 -0800

    Add stylesheet and widgets

--- server.R
+++ server.R
@@ -1,6 +1,16 @@
+library("ggplot2")
+
+
 my.server <- function(input, output) { 
     
-    
+  # generates a plot of measurments vs stats
+  output$plot <- renderPlot({
+    graph <- ggplot(data = "something from reactive table") +
+      geom_bar(mapping = aes(x = ..., y = ..., fill = ..), stat = "identity") + 
+      xlab("X label som,ething") +
+      ylab("Y label something")
+    return(graph)
+  })
 }
 
 shinyServer(my.server)
